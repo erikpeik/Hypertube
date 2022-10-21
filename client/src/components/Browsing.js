ee240cc5b066188aa2001898fac6c9938c2dc2e4
Gabor Horvath-Ulenius <mobahug@gmail.com>
2022-10-21 16:03:10 +0300

    working on browse to make responsive

--- client/src/components/Browsing.js
+++ client/src/components/Browsing.js
@@ -1,28 +1,55 @@
-import browsingService from "../services/browsingService"
-import { useState, useEffect } from "react"
+import browsingService from "../services/browsingService";
+import { useState, useEffect } from "react";
 import Loader from "./Loader";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 
 const Browsing = () => {
-	const [movies, setMovies] = useState(null)
-	useEffect(() => {
-		browsingService.getMovies().then((movies) => {
-			console.log(movies.data)
-			setMovies(movies.data.movies || [])
-		})
-	}, [])
-	if (!movies) return <Loader />
+  const [movies, setMovies] = useState(null);
+  useEffect(() => {
+    browsingService.getMovies().then((movies) => {
+      console.log(movies.data);
+      setMovies(movies.data.movies || []);
+    });
+  }, []);
+  if (!movies) return <Loader />;
 
-	return (
-		<>
-			<h1>Browsing</h1>
-			{movies.map(movie => (
-			<div key={movie.id}>
-					<h2>{movie.title}</h2>
-					<img src={movie.medium_cover_image} alt={movie.title} />
-				</div>
-			))}
-		</>
-	)
-}
+  return (
+    <Box
+      container
+      spacing={3}
+      style={{
+        direction: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        display: "flex",
+        flexWrap: "wrap",
+      }}
+    >
+      {movies.map((movie) => (
+        <Box key={movie.id} item xs={3}>
+          <Card sx={{ flexGrow: 1, height: 345, width: 245 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                image={movie.medium_cover_image}
+                alt={movie.title}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h7">
+                  {movie.title}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Box>
+      ))}
+    </Box>
+  );
+};
 
-export default Browsing
+export default Browsing;
