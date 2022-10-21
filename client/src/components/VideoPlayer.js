0bee32b9bbcad6a550051226ca909b76d67bfe12
Anamaria Camarasan <acamaras@student.hive.fi>
2022-10-21 17:55:21 +0300

    player done, slider to be fixed

--- client/src/components/VideoPlayer.js
+++ client/src/components/VideoPlayer.js
@@ -1,12 +1,14 @@
 import "../css/VideoPlayer.css";
 import * as React from "react";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import ReactPlayer from "react-player";
 import screenfull from "screenfull";
 import Container from "@mui/material/Container";
 import ControlIcons from "./control/ControlIcons";
 
 const VideoPlayer = () => {
+	const playerRef = useRef(null);
+	const playerDivRef = useRef(null);
 	const [playerState, setPlayerState] = useState({
 		playing: true,
 		mute: true,
@@ -16,7 +18,7 @@ const VideoPlayer = () => {
 		seeking: false,
 	});
 
-	const { playing, mute, volume, playerbackRate, played, seeking } =
+	const { playing, mute, volume, playerbackRate, played} =
 		playerState;
 
 	const handlePlayAndPause = () => {
@@ -26,19 +28,123 @@ const VideoPlayer = () => {
 		});
 	};
 
+	const handleRewind = () => {
+		playerRef.current.seekTo(
+			playerRef.current.getCurrentTime() - 10,
+			`seconds`
+		);
+	};
+
+	const handleFastForward = () => {
+		playerRef.current.seekTo(
+			playerRef.current.getCurrentTime() + 30,
+			`seconds`
+		);
+	};
+
+	const handlePlayerProgress = (state) => {
+		console.log("onProgress", state);
+		if (!playerState.seeking) {
+			setPlayerState({ ...playerState, ...state });
+		}
+		console.log("afterProgress", state);
+	};
+
+	const handlePlayerSeek = (newValue) => {
+		setPlayerState({ ...playerState, played: parseFloat(newValue / 100) });
+		playerRef.current.seekTo(parseFloat(newValue / 100));
+	};
+
+	const handlePlayerMouseSeekUp = (newValue) => {
+		setPlayerState({ ...playerState, seeking: false });
+		playerRef.current.seekTo(newValue / 100);
+	};
+
+	const format = (seconds) => {
+		if (isNaN(seconds)) {
+			return "00:00";
+		}
+
+		const date = new Date(seconds * 1000);
+		const hh = date.getUTCHours();
+		const mm = date.getUTCMinutes();
+		const ss = date.getUTCSeconds().toString().padStart(2, "0");
+
+		if (hh) {
+			return `${hh}:${mm.toString().padStart(2, "0")}:${ss}`;
+		} else {
+			return `${mm}:${ss}`;
+		}
+	};
+
+	const currentPlayerTime = playerRef.current
+		? playerRef.current.getCurrentTime()
+		: "00:00";
+	const movieDuration = playerRef.current
+		? playerRef.current.getDuration()
+		: "00:00";
+	const playedTime = format(currentPlayerTime);
+	const fullMovieTime = format(movieDuration);
+
+	const handleMuting = () => {
+		setPlayerState({ ...playerState, muted: !playerState.muted });
+	};
+	const handleVolumeChange = (e, newValue) => {
+		setPlayerState({
+			...playerState,
+			volume: parseFloat(newValue / 100),
+			mute: newValue === 0 ? true : false,
+		});
+	};
+
+	const handleVolumeSeek = (e, newValue) => {
+		setPlayerState({
+			...playerState,
+			volume: parseFloat(newValue / 100),
+			mute: newValue === 0 ? true : false,
+		});
+	};
+	const handlePlayerRate = (rate) => {
+		setPlayerState({ ...playerState, playerbackRate: rate });
+	};
+
+	const handleFullScreenMode = () => {
+		screenfull.toggle(playerDivRef.current);
+	};
 
 	return (
 		<>
 			<Container maxWidth="md">
-				<div className="playerDiv">
+				<div className="playerDiv" ref={playerDivRef}>
 					<ReactPlayer
 						width={"100%"}
 						height="100%"
 						url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
+						ref={playerRef}
 						playing={playing}
 						muted={mute}
+						onProgress={handlePlayerProgress}
+						playbackRate={playerbackRate}
 					/>
-					<ControlIcons playandpause={handlePlayAndPause} playing={playing}/>
+					<ControlIcons
+						playandpause={handlePlayAndPause}
+						playing={playing}
+						rewind={handleRewind}
+						fastForward={handleFastForward}
+						played={played}
+						onSeek={handlePlayerSeek}
+						onSeekMouseUp={handlePlayerMouseSeekUp}
+						playedTime={playedTime}
+						fullMovieTime={fullMovieTime}
+						muting={handleMuting}
+						muted={mute}
+						volume={volume}
+						volumeChange={handleVolumeChange}
+						volumeSeek={handleVolumeSeek}
+						playerbackRate={playerbackRate}
+						playRate={handlePlayerRate}
+						fullScreenMode={handleFullScreenMode}
+					/>
 				</div>
 			</Container>
 		</>
