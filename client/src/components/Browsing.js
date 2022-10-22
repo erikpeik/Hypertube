550a414ebcbbc28aedeb342f4339005f260c55c5
Gabor Horvath-Ulenius <mobahug@gmail.com>
2022-10-22 17:22:23 +0300

    styling

--- client/src/components/Browsing.js
+++ client/src/components/Browsing.js
@@ -7,6 +7,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
+import "./style.css";
 
 const Browsing = () => {
   const [movies, setMovies] = useState(null);
@@ -32,20 +33,28 @@ const Browsing = () => {
     >
       {movies.map((movie) => (
         <Box key={movie.id} item xs={3}>
-          <Card sx={{ flexGrow: 1, height: 345, width: 245 }}>
+          <Card
+            class="container"
+            sx={{ margin: 2, flexGrow: 1, height: 345, width: 245 }}
+          >
             <CardActionArea>
               <CardMedia
                 component="img"
                 image={movie.medium_cover_image}
                 alt={movie.title}
               />
-              <CardContent>
+              <CardContent className="newsletter">
                 <Typography gutterBottom variant="h7">
                   {movie.title}
                 </Typography>
               </CardContent>
             </CardActionArea>
           </Card>
+          <div class="container">
+            <div class="newsletter">
+              <p>weee!</p>
+            </div>
+          </div>
         </Box>
       ))}
     </Box>
