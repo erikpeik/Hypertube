761abdbd160cd58b59cf7b83e2ec76ae162c120b
Erik Mende <emende@student.hive.fi>
2022-10-24 17:21:04 +0300

    Small fix to get rid of that not defined variable

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,6 +1,8 @@
 import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 
+const baseUrl = 'http://localhost:3001/api/browsing'
+
 const useFetch = (query, page) => {
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(false);
@@ -10,7 +12,7 @@ const useFetch = (query, page) => {
 		try {
 			setLoading(true)
 			setError(false)
-			const { data } = await axios.get(url)
+			const { data } = await axios.get(`${baseUrl}`)
 			setMovies(prevMovies => [...prevMovies, ...data.results])
 			setLoading(false)
 		} catch (err) {
