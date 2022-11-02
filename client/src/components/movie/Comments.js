8387d59446b6121ad4c4c88ab160b2aed0108018
Gabor Horvath-Ulenius <mobahug@gmail.com>
2022-11-02 15:34:32 +0200

    handled case when logged in user click to own prof pic in comment section

--- client/src/components/movie/Comments.js
+++ client/src/components/movie/Comments.js
@@ -1,10 +1,10 @@
-import "../../css/movie.css";
-import React, { useEffect, useState } from "react";
-import { Divider, Avatar, Grid, Paper } from "@mui/material";
-import { Container } from "@mui/system";
-import NewComment from "./NewComment";
-import commentService from "../../services/commentService";
-import { format } from "timeago.js";
+import '../../css/movie.css';
+import React, { useEffect, useState } from 'react';
+import { Divider, Avatar, Grid, Paper } from '@mui/material';
+import { Container } from '@mui/system';
+import NewComment from './NewComment';
+import commentService from '../../services/commentService';
+import { format } from 'timeago.js';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
@@ -17,18 +17,18 @@ const Comments = ({ movieId, t }) => {
 
 	useEffect(() => {
 		commentService.getComments(movieId).then((response) => {
-			setComments(response || "");
+			setComments(response || '');
 		});
 	}, [movieId, refresh]);
 
-	console.log(comments)
-	console.log(profileData)
+	console.log(comments);
+	console.log(profileData);
 
 	return (
 		<>
 			<Container maxWidth="md" sx={{ pt: 5, pb: 5 }}>
 				<NewComment movieId={movieId} setRefresh={setRefresh} t={t} />
-				<Paper style={{ padding: "20px 20px", marginTop: 10 }}>
+				<Paper style={{ padding: '20px 20px', marginTop: 10 }}>
 					{comments.length > 0 ? (
 						comments.sort((a, b) => b.id - a.id) &&
 						comments.map((c) => {
@@ -39,14 +39,26 @@ const Comments = ({ movieId, t }) => {
 									spacing={2}
 									key={c.id}
 								>
-									<Grid item>
-										<Link to={'/profile/' + c?.id}>
-											<Avatar
-												alt="Remy Sharp"
-												src={c.user_pic}
-											/>
-										</Link>
-									</Grid>
+									{c?.user_id === profileData.id ? (
+										<Grid item>
+											<Link to={'/profile'}>
+												<Avatar
+													alt="Remy Sharp"
+													src={c.user_pic}
+												/>
+											</Link>
+										</Grid>
+									) : (
+										<Grid item>
+											<Link to={'/profile/' + c?.id}>
+												<Avatar
+													alt="Remy Sharp"
+													src={c.user_pic}
+												/>
+											</Link>
+										</Grid>
+									)}
+
 									<Grid
 										justifyContent="left"
 										item
@@ -56,25 +68,25 @@ const Comments = ({ movieId, t }) => {
 										<h4
 											style={{
 												margin: 0,
-												textAlign: "left",
+												textAlign: 'left',
 											}}
 										>
 											{c.username}
 										</h4>
-										<p style={{ textAlign: "left" }}>
-											{c.comment}{" "}
+										<p style={{ textAlign: 'left' }}>
+											{c.comment}{' '}
 										</p>
 										<p
 											style={{
-												textAlign: "left",
-												color: "gray",
+												textAlign: 'left',
+												color: 'gray',
 											}}
 										>
 											{format(c.created_at)}
 										</p>
 										<Divider
 											variant="fullWidth"
-											style={{ margin: "30px 0" }}
+											style={{ margin: '30px 0' }}
 										/>
 									</Grid>
 								</Grid>
@@ -83,11 +95,11 @@ const Comments = ({ movieId, t }) => {
 					) : (
 						<p
 							style={{
-								textAlign: "left",
-								color: "gray",
+								textAlign: 'left',
+								color: 'gray',
 							}}
 						>
-							{t("movie.2")}
+							{t('movie.2')}
 						</p>
 					)}
 				</Paper>
