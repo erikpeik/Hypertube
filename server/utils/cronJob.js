3f1a0cb436d110d346f581a7e3189033f6cca45b
Erik Mende <emende@student.hive.fi>
2022-11-08 15:48:14 +0200

    Updated logic of getting movies and the dates on crontab
    Added timezone to postgres with help of Gabor

--- server/utils/cronJob.js
+++ server/utils/cronJob.js
@@ -4,45 +4,36 @@ const path = require('node:path');
 
 module.exports = function (app, pool, axios) {
 	function get() {
+		const current_date = Date.now();
+		const month_ago = new Date(current_date - 2592000000)
+			.toISOString()
+			.slice(0, 19)
+			.replace('T', ' ');
 		let sql = `SELECT t.imdb_id, t.created_at, d.path
-				FROM (
-					SELECT * FROM movies_watched
-					WHERE (imdb_id, created_at) in (
-						SELECT imdb_id, MAX(created_at)
-						FROM movies_watched
-						GROUP BY imdb_id
-						)
-					) t
-				LEFT JOIN downloads d ON d.imdb_id = t.imdb_id`;
-		pool.query(sql, (err, result) => {
+		FROM (
+			SELECT * FROM movies_watched
+			WHERE (imdb_id, created_at) in (
+				SELECT imdb_id, MAX(created_at)
+				FROM movies_watched
+				GROUP BY imdb_id
+			)
+		) t
+		INNER JOIN downloads d ON d.imdb_id = t.imdb_id
+		WHERE t.created_at <= $1 AND d.completed = 'YES'`;
+		pool.query(sql, [month_ago], (err, result) => {
 			if (err) throw err;
 			console.log(result.rows);
 			result.rows.forEach((element) => {
-				let current_date = Date.now();
-				// console.log('CURRENTDATE: ', current_date);
-				// console.log('OLDDATE: ', element.created_at);
-				let date1 = new Date(element.created_at).getTime();
-				// console.log('OLDDATE: ', date1);
-				let addedOldDate = date1 + 2592000000;
-				// console.log('+30days: ', addedOldDate);
-				if (addedOldDate < current_date) {
-					console.log('older than 30 days'); // go back in time
-					sql = `DELETE FROM movies_watched WHERE imdb_id = $1`;
-					pool.query(sql, [element.imdb_id]);
-					const element_path = element.path.split('/');
-					const folder_path = path.resolve(
-						__dirname,
-						`../movies/${element_path[1]}`
-					);
-					console.log('folder_path', folder_path);
-					if (fs.existsSync(folder_path)) {
-						fs.rmSync(folder_path, { recursive: true });
-					}
-					sql = `DELETE FROM downloads WHERE imdb_id = $1`;
-					pool.query(sql, [element.imdb_id]);
-				} else {
-					console.log('not older than 30 days');
+				const element_path = element.path.split('/');
+				const folder_path = path.resolve(
+					__dirname,
+					`../movies/${element_path[1]}`
+				);
+				if (fs.existsSync(folder_path)) {
+					fs.rmSync(folder_path, { recursive: true });
 				}
+				sql = `DELETE FROM downloads WHERE imdb_id = $1`;
+				pool.query(sql, [element.imdb_id]);
 			});
 		});
 	}
