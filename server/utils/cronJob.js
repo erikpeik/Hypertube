2652bbba7c94e4a7185b4c1f6b36d6f5f748d280
Erik Mende <emende@student.hive.fi>
2022-11-08 13:33:40 +0200

    Changes to crontab and queries

--- server/utils/cronJob.js
+++ server/utils/cronJob.js
@@ -1,30 +1,45 @@
 const cron = require('node-cron');
-module.exports = function (app, pool, axios) {
+const fs = require('fs');
+const path = require('node:path');
 
+module.exports = function (app, pool, axios) {
 	function get() {
-		let sql = `SELECT created_at FROM movies_watched`;
+		let sql = `SELECT t.imdb_id, t.created_at, d.path
+				FROM (
+					SELECT * FROM movies_watched
+					WHERE (imdb_id, created_at) in (
+						SELECT imdb_id, MAX(created_at)
+						FROM movies_watched
+						GROUP BY imdb_id
+						)
+					) t
+				LEFT JOIN downloads d ON d.imdb_id = t.imdb_id`;
 		pool.query(sql, (err, result) => {
 			if (err) throw err;
 			console.log(result.rows);
 			result.rows.forEach((element) => {
-				var today = new Date();
-				var currentDate =
-					today.getFullYear() +
-					'/' +
-					(today.getMonth() + 1) +
-					'/' +
-					today.getDate();
-				let date2 = new Date(currentDate).getTime();
-				console.log('CURRENTDATE: ', date2);
-				console.log('OLDDATE: ', element.created_at);
+				let current_date = Date.now();
+				// console.log('CURRENTDATE: ', current_date);
+				// console.log('OLDDATE: ', element.created_at);
 				let date1 = new Date(element.created_at).getTime();
-				console.log('OLDDATE: ', date1);
+				// console.log('OLDDATE: ', date1);
 				let addedOldDate = date1 + 2592000000;
-				console.log('+30days: ', addedOldDate);
-				if (date1 < date2) {
+				// console.log('+30days: ', addedOldDate);
+				if (addedOldDate < current_date) {
 					console.log('older than 30 days'); // go back in time
-					sql = `DELETE FROM movies_watched WHERE created_at = $1`;
-					pool.query(sql, [element.created_at]);
+					sql = `DELETE FROM movies_watched WHERE imdb_id = $1`;
+					pool.query(sql, [element.imdb_id]);
+					const element_path = element.path.split('/');
+					const folder_path = path.resolve(
+						__dirname,
+						`../movies/${element_path[1]}`
+					);
+					console.log('folder_path', folder_path);
+					if (fs.existsSync(folder_path)) {
+						fs.rmSync(folder_path, { recursive: true });
+					}
+					sql = `DELETE FROM downloads WHERE imdb_id = $1`;
+					pool.query(sql, [element.imdb_id]);
 				} else {
 					console.log('not older than 30 days');
 				}
@@ -35,7 +50,7 @@ module.exports = function (app, pool, axios) {
 		cron.schedule(
 			'0 8 * * *', // '*/1 * * * * *', // every second for testing
 			function () {
-				get()
+				get();
 			},
 			{
 				scheduled: true,
