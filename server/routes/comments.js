1d1c2c067ac1d599c23bdc00e113a7c6672074fa
Pekka Lehtikangas <plehtika@student.hive.fi>
2022-11-09 16:52:35 +0200

    comment tests added

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -2,6 +2,10 @@ module.exports = function (app, pool) {
 	app.post("/api/newcomment/:id", async (request, response) => {
 		const imdb_id = request.params.id;
 		const { comment } = request.body;
+		if (!comment || comment === '')
+			return response.send("Comment missing")
+		if (imdb_id.length > 12)
+			return response.send("Faulty imdb_id")
 		if (comment.length > 500)
 			return response.send("Maximum length for comments is 500 characters.")
 
@@ -9,14 +13,14 @@ module.exports = function (app, pool) {
 		if (cookie) {
 			let sql = "SELECT * FROM users WHERE token = $1";
 			const { rows } = await pool.query(sql, [cookie]);
-			const user_id = rows[0]["id"];
-			const username = rows[0]["username"];
 			if (rows.length) {
+				const user_id = rows[0]["id"];
+				const username = rows[0]["username"];
 				sql = "SELECT * FROM user_pictures WHERE user_id = $1";
-				const { rows } = await pool.query(sql, [user_id]);
+				const picture = await pool.query(sql, [user_id]);
 				let picture_path;
-				if (rows[0] !== undefined)
-					picture_path = rows[0]["picture_data"];
+				if (picture.rows[0] !== undefined)
+					picture_path = picture.rows[0]["picture_data"];
 				else {
 					picture_path =
 						"http://localhost:3001/images/file-1666255644329.jpg";
@@ -24,17 +28,11 @@ module.exports = function (app, pool) {
 				try {
 					sql =
 						"INSERT INTO comments (user_id, username, user_pic, imdb_id, comment) VALUES ($1, $2, $3, $4, $5) RETURNING *";
-					await pool.query(sql, [
-						user_id,
-						username,
-						picture_path,
-						imdb_id,
-						comment,
-					]);
+					await pool.query(sql, [user_id, username, picture_path, imdb_id, comment]);
 					response.send(true);
-					return;
 				} catch (error) {
 					console.log("ERROR: ", error);
+					response.send("")
 				}
 			} else response.send("");
 		} else response.send("");
@@ -42,14 +40,14 @@ module.exports = function (app, pool) {
 
 	app.get("/api/getcomments/:token", async (request, response) => {
 		const imdb_id = request.params.token;
-		if (imdb_id !== "") {
-			try {
-				sql = "SELECT * FROM comments WHERE imdb_id = $1";
-				const comments = await pool.query(sql, [imdb_id]);
-				response.send(comments.rows);
-			} catch (error) {
-				console.log("ERROR: ", error);
-			}
-		} else response.send(false);
+		if (imdb_id.length > 12)
+			return response.send("Faulty imdb_id")
+		try {
+			sql = "SELECT * FROM comments WHERE imdb_id = $1";
+			const comments = await pool.query(sql, [imdb_id]);
+			response.send(comments.rows);
+		} catch (error) {
+			console.log("ERROR: ", error);
+		}
 	});
 };
