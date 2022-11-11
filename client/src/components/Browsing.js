f80f53b96e31b8b9e76f135fca97131e9bec43a4
Gabor Horvath-Ulenius <mobahug@gmail.com>
2022-11-11 16:53:06 +0200

    separating infinite and pagination

--- client/src/components/Browsing.js
+++ client/src/components/Browsing.js
@@ -7,7 +7,7 @@ import SearchBar from './browsing/SearchBar';
 import MovieList from './browsing/MovieList';
 import { useSelector, useDispatch } from 'react-redux';
 import Loader from './Loader';
-
+import Infinite from './infinite';
 import Pagination from './Pagination';
 
 const Browsing = ({ t }) => {
@@ -48,24 +48,9 @@ const Browsing = ({ t }) => {
 	// 	[browsingSettings]
 	// 	);
 
-	const handleObserver = useCallback((entries) => {
-		const target = entries[0];
-		if (target.isIntersecting) {
-			setPage((prev) => prev + 1);
-		}
-	}, []);
 
-	useEffect(() => {
-		const options = {
-			root: null,
-			rootMargin: '20px',
-			threshold: 0,
-		};
-		const observer = new IntersectionObserver(handleObserver, options);
-		if (loader.current) observer.observe(loader.current);
-	}, [handleObserver]);
 
-	if (!movies || !profileData) return <Loader />;
+	if (!movies || profileData === null) return <Loader />;
 
 	const indexOfLastRecord = currentPage * recordsPerPage;
 	const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
@@ -74,38 +59,57 @@ const Browsing = ({ t }) => {
 		indexOfLastRecord
 	);
 	const nPages = Math.ceil(infinite_movies.length / recordsPerPage);
-
-	return (
-		<Container
-			sx={{
-				maxWidth: 1080,
-				display: 'flex',
-				flexDirection: 'column',
-				alignItems: 'center',
-			}}
-		>
-			<SearchBar
-				t={t}
-				browsingSettings={browsingSettings}
-				setBrowsingSettings={setBrowsingSettings}
-			/>
-			{profileData?.infinite_scroll === 'YES' ? (
+	console.log(profileData.infinite_scroll);
+	if (profileData.infinite_scroll === 'NO') {
+		return (
+			<Container
+				sx={{
+					maxWidth: 1080,
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+				}}
+			>
+				<SearchBar
+					t={t}
+					browsingSettings={browsingSettings}
+					setBrowsingSettings={setBrowsingSettings}
+				/>
+				<MovieList movies={currentRecords} />
+				<Pagination
+					nPages={nPages}
+					currentPage={currentPage}
+					setCurrentPage={setCurrentPage}
+				/>
+				{loading && <LoaderDots />}
+				{error && <p>Error!</p>}
+				<div ref={loader} />
+			</Container>
+		);
+	}
+	if (profileData.infinite_scroll === 'YES') {
+		return (
+			<Container
+				sx={{
+					maxWidth: 1080,
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+				}}
+			>
+				<SearchBar
+					t={t}
+					browsingSettings={browsingSettings}
+					setBrowsingSettings={setBrowsingSettings}
+				/>
 				<MovieList movies={movies} />
-			) : (
-				<>
-					<MovieList movies={currentRecords} />
-					<Pagination
-						nPages={nPages}
-						currentPage={currentPage}
-						setCurrentPage={setCurrentPage}
-					/>
-				</>
-			)}
-			{loading && <LoaderDots />}
-			{error && <p>Error!</p>}
-			<div ref={loader} />
-		</Container>
-	);
+				<Infinite/>
+				{loading && <LoaderDots />}
+				{error && <p>Error!</p>}
+				<div ref={loader} />
+			</Container>
+		);
+	}
 };
 
 export default Browsing;
